f45b615f604ab0e0c151cdd6e51b01711391ace4
GuilhermeVictor <98266333+GuilhermeVRF@users.noreply.github.com>
2024-01-09 18:52:48 -0300

    Add files via upload

--- JabocCafeteria/src/jaboc_UI/JabocUI_Utilidades/JabocUI_popUp/PopUp_GerenciarFuncionarios.java
+++ JabocCafeteria/src/jaboc_UI/JabocUI_Utilidades/JabocUI_popUp/PopUp_GerenciarFuncionarios.java
@@ -9,41 +9,25 @@ import jaboc_UI.Administrador.interface_admApagarFuncionario;
 import jaboc_UI.Administrador.interface_admEditarFuncionario;
 import jaboc_UI.Funcionarios.interface_exibirFuncionarios;
 import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
-import java.awt.geom.RoundRectangle2D;
 import raven.glasspanepopup.GlassPanePopup;
 
 /**
  *
- * @author eeuar
+ * @author guilh
  */
-public class PopUp_GerenciarFuncionarios extends javax.swing.JPanel {
-
+public class PopUp_GerenciarFuncionarios extends javax.swing.JPanel{
     private Conta_Funcionario funcionarioGerenciavel;
     private final interface_exibirFuncionarios EXIBIR_FUNCIONARIOS;
-
     /**
-     * Creates new form PopUp_GerenciaProdutos
+     * Creates new form PopUp_GerenciarFuncionarios
+     * @param funcionarioGerenciavel
      */
-    public PopUp_GerenciarFuncionarios(Conta_Funcionario funcionarioGerenciavel, interface_exibirFuncionarios exibirFuncionarios) {
+    
+    public PopUp_GerenciarFuncionarios(Conta_Funcionario funcionarioGerenciavel, interface_exibirFuncionarios exibirFuncionarios){
         this.funcionarioGerenciavel = funcionarioGerenciavel;
         this.EXIBIR_FUNCIONARIOS = exibirFuncionarios;
         initComponents();
-        setOpaque(false);
     }
-
-    @Override
-    protected void paintComponent(Graphics g) {
-        Graphics2D g2 = (Graphics2D) g.create();
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.setColor(getBackground());
-        g2.fill(new RoundRectangle2D.Double(0, 0, getWidth(), getHeight(), 20, 20));
-        g2.dispose();
-        super.paintComponent(g); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
-    }
-
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -57,8 +41,8 @@ public class PopUp_GerenciarFuncionarios extends javax.swing.JPanel {
         jLabel1 = new javax.swing.JLabel();
         bPopUpExit = new jaboc_UI.jabocUI_Utilidades.ButtonCirculo();
         panel2 = new jaboc_UI.jabocUI_Utilidades.Panel();
-        bEdiatr_Funcionario = new jaboc_UI.jabocUI_Utilidades.Button();
-        bExcluir_Funcionario = new jaboc_UI.jabocUI_Utilidades.Button();
+        bEdiatr_Funcionario = new jaboc_UI.jabocUI_Utilidades.ButtonCirculo();
+        bExcluir_Funcionario = new jaboc_UI.jabocUI_Utilidades.ButtonCirculo();
 
         panel1.setBackground(new java.awt.Color(255, 255, 255));
 
@@ -81,9 +65,8 @@ public class PopUp_GerenciarFuncionarios extends javax.swing.JPanel {
         panel2.setBackground(new java.awt.Color(255, 255, 255));
 
         bEdiatr_Funcionario.setBackground(new java.awt.Color(79, 84, 101));
-        bEdiatr_Funcionario.setForeground(new java.awt.Color(255, 255, 255));
-        bEdiatr_Funcionario.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icons/i_editar1.png"))); // NOI18N
-        bEdiatr_Funcionario.setText(" Editar ");
+        bEdiatr_Funcionario.setBorder(null);
+        bEdiatr_Funcionario.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icons/i_editar3.png"))); // NOI18N
         bEdiatr_Funcionario.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseEntered(java.awt.event.MouseEvent evt) {
                 bEdiatr_FuncionarioMouseEntered(evt);
@@ -99,9 +82,11 @@ public class PopUp_GerenciarFuncionarios extends javax.swing.JPanel {
         });
 
         bExcluir_Funcionario.setBackground(new java.awt.Color(79, 84, 101));
-        bExcluir_Funcionario.setForeground(new java.awt.Color(255, 255, 255));
-        bExcluir_Funcionario.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icons/i_lixo4.png"))); // NOI18N
-        bExcluir_Funcionario.setText("Deletar");
+        bExcluir_Funcionario.setBorder(null);
+        bExcluir_Funcionario.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icons/i_lixo2.png"))); // NOI18N
+        bExcluir_Funcionario.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        bExcluir_Funcionario.setIconTextGap(0);
+        bExcluir_Funcionario.setInheritsPopupMenu(true);
         bExcluir_Funcionario.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseEntered(java.awt.event.MouseEvent evt) {
                 bExcluir_FuncionarioMouseEntered(evt);
@@ -121,20 +106,20 @@ public class PopUp_GerenciarFuncionarios extends javax.swing.JPanel {
         panel2Layout.setHorizontalGroup(
             panel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(panel2Layout.createSequentialGroup()
-                .addGap(8, 8, 8)
-                .addGroup(panel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(bEdiatr_Funcionario, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(bExcluir_Funcionario, javax.swing.GroupLayout.DEFAULT_SIZE, 254, Short.MAX_VALUE))
-                .addContainerGap())
+                .addGap(30, 30, 30)
+                .addComponent(bEdiatr_Funcionario, javax.swing.GroupLayout.PREFERRED_SIZE, 84, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 41, Short.MAX_VALUE)
+                .addComponent(bExcluir_Funcionario, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(30, 30, 30))
         );
         panel2Layout.setVerticalGroup(
             panel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panel2Layout.createSequentialGroup()
-                .addContainerGap(8, Short.MAX_VALUE)
-                .addComponent(bEdiatr_Funcionario, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(bExcluir_Funcionario, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(8, 8, 8))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panel2Layout.createSequentialGroup()
+                .addContainerGap(10, Short.MAX_VALUE)
+                .addGroup(panel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(bExcluir_Funcionario, javax.swing.GroupLayout.DEFAULT_SIZE, 85, Short.MAX_VALUE)
+                    .addComponent(bEdiatr_Funcionario, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(10, 10, 10))
         );
 
         javax.swing.GroupLayout panel1Layout = new javax.swing.GroupLayout(panel1);
@@ -144,15 +129,15 @@ public class PopUp_GerenciarFuncionarios extends javax.swing.JPanel {
             .addGroup(panel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 290, Short.MAX_VALUE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panel1Layout.createSequentialGroup()
                         .addGap(0, 0, Short.MAX_VALUE)
                         .addComponent(bPopUpExit, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap())
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(panel1Layout.createSequentialGroup()
+                .addGap(32, 32, 32)
                 .addComponent(panel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(17, 17, 17))
+                .addContainerGap(25, Short.MAX_VALUE))
         );
         panel1Layout.setVerticalGroup(
             panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -160,21 +145,21 @@ public class PopUp_GerenciarFuncionarios extends javax.swing.JPanel {
                 .addContainerGap()
                 .addComponent(bPopUpExit, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(panel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(16, Short.MAX_VALUE))
+                .addContainerGap(21, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(panel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(panel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(panel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(panel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -187,13 +172,14 @@ public class PopUp_GerenciarFuncionarios extends javax.swing.JPanel {
     }//GEN-LAST:event_bEdiatr_FuncionarioMouseEntered
 
     private void bEdiatr_FuncionarioMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bEdiatr_FuncionarioMouseExited
-        bEdiatr_Funcionario.setBackground(new Color(79, 84, 101));
+        bEdiatr_Funcionario.setBackground(new Color(79,84,101));
     }//GEN-LAST:event_bEdiatr_FuncionarioMouseExited
 
     private void bEdiatr_FuncionarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bEdiatr_FuncionarioActionPerformed
         GlassPanePopup.closePopupLast();
-        interface_admEditarFuncionario i_admEditarFuncionario = new interface_admEditarFuncionario(this.funcionarioGerenciavel, this.EXIBIR_FUNCIONARIOS);
-        i_admEditarFuncionario.setVisible(true);    
+        this.EXIBIR_FUNCIONARIOS.dispose();
+        interface_admEditarFuncionario i_admEditarFuncionario = new interface_admEditarFuncionario(this.funcionarioGerenciavel);
+        i_admEditarFuncionario.setVisible(true);        
     }//GEN-LAST:event_bEdiatr_FuncionarioActionPerformed
 
     private void bExcluir_FuncionarioMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bExcluir_FuncionarioMouseEntered
@@ -201,20 +187,22 @@ public class PopUp_GerenciarFuncionarios extends javax.swing.JPanel {
     }//GEN-LAST:event_bExcluir_FuncionarioMouseEntered
 
     private void bExcluir_FuncionarioMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bExcluir_FuncionarioMouseExited
-        bExcluir_Funcionario.setBackground(new Color(79, 84, 101));
+        bExcluir_Funcionario.setBackground(new Color(79,84,101));
     }//GEN-LAST:event_bExcluir_FuncionarioMouseExited
 
     private void bExcluir_FuncionarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bExcluir_FuncionarioActionPerformed
         GlassPanePopup.closePopupLast();
-        interface_admApagarFuncionario i_admApagarFuncionario = new interface_admApagarFuncionario(this.funcionarioGerenciavel, this.EXIBIR_FUNCIONARIOS);
-        i_admApagarFuncionario.setVisible(true);    
-
+        this.EXIBIR_FUNCIONARIOS.dispose();
+        interface_admApagarFuncionario i_admApagarFuncionario = new interface_admApagarFuncionario(this.funcionarioGerenciavel);
+        i_admApagarFuncionario.setVisible(true);       
     }//GEN-LAST:event_bExcluir_FuncionarioActionPerformed
 
+    
+   
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private jaboc_UI.jabocUI_Utilidades.Button bEdiatr_Funcionario;
-    private jaboc_UI.jabocUI_Utilidades.Button bExcluir_Funcionario;
+    private jaboc_UI.jabocUI_Utilidades.ButtonCirculo bEdiatr_Funcionario;
+    private jaboc_UI.jabocUI_Utilidades.ButtonCirculo bExcluir_Funcionario;
     private jaboc_UI.jabocUI_Utilidades.ButtonCirculo bPopUpExit;
     private javax.swing.JLabel jLabel1;
     private jaboc_UI.jabocUI_Utilidades.Panel panel1;
